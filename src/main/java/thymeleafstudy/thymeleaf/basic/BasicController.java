54d95fb40680ba484c0ae5d2d9a4d748eeea54c9
최영찬 <gks54410@gmail.com>
2022-08-15 16:31:17 +0900

    Feat: Thymeleaf 기본, 편의 객체

--- src/main/java/thymeleafstudy/thymeleaf/basic/BasicController.java
+++ src/main/java/thymeleafstudy/thymeleaf/basic/BasicController.java
@@ -1,10 +1,12 @@
 package thymeleafstudy.thymeleaf.basic;
 
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -42,6 +44,19 @@ public class BasicController {
         model.addAttribute("userMap", map);
 
         return "basic/variable";
+    }
+
+    @GetMapping("/basic-objects")
+    public String basicObjects(HttpSession session) {
+        session.setAttribute("sessionData", "Hello Session");
+        return "basic/baisc-objects";
+    }
+
+    @Component("helloBean")
+    static class HelloBean {
+        public String hello(String data) {
+            return "Hello" + data;
+        }
     }
 
     public static class User {
