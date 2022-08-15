938879195efb1bed4d894df06e46640b4f93b954
최영찬 <gks54410@gmail.com>
2022-08-15 18:41:28 +0900

    Feat: th:block

--- src/main/java/thymeleafstudy/thymeleaf/basic/BasicController.java
+++ src/main/java/thymeleafstudy/thymeleaf/basic/BasicController.java
@@ -104,7 +104,13 @@ public class BasicController {
         model.addAttribute("data", "Spring!");
 
         return "basic/comments";
+    }
 
+    @GetMapping("/block")
+    public String block(Model model) {
+        addUsers(model);
+
+        return "basic/block";
     }
 
     private void addUsers(Model model) {
