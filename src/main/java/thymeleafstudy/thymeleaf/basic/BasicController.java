b0f6d00859f3919d8b1c7875e7e3693b4d17025b
최영찬 <gks54410@gmail.com>
2022-08-15 16:23:00 +0900

    Feat: Thymeleaf 변수

--- src/main/java/thymeleafstudy/thymeleaf/basic/BasicController.java
+++ src/main/java/thymeleafstudy/thymeleaf/basic/BasicController.java
@@ -5,6 +5,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 @Controller
 @RequestMapping("/basic")
 public class BasicController {
@@ -20,4 +23,43 @@ public class BasicController {
         model.addAttribute("data", "Hello <b>Spring!</b>");
         return "basic/text-unescaped";
     }
+
+    @GetMapping("/variable")
+    public String variable(Model model) {
+        User userA = new User("userA", 10);
+        User userB = new User("userA", 20);
+
+        ArrayList<User> users = new ArrayList<>();
+        users.add(userA);
+        users.add(userB);
+
+        HashMap<String, User> map = new HashMap<>();
+        map.put("userA", userA);
+        map.put("userB", userB);
+
+        model.addAttribute("user", userA);
+        model.addAttribute("users", users);
+        model.addAttribute("userMap", map);
+
+        return "basic/variable";
+    }
+
+    public static class User {
+        private String username;
+        private int age;
+
+        public User(String username, int age) {
+            this.username = username;
+            this.age = age;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public int getAge() {
+            return age;
+        }
+    }
+
 }
