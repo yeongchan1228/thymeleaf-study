e172883c0eff44f3698e7dd966050f2a45219939
최영찬 <gks54410@gmail.com>
2022-08-15 18:54:09 +0900

    Feat: 자바스크립트 인라인

--- src/main/java/thymeleafstudy/thymeleaf/basic/BasicController.java
+++ src/main/java/thymeleafstudy/thymeleaf/basic/BasicController.java
@@ -113,6 +113,14 @@ public class BasicController {
         return "basic/block";
     }
 
+    @GetMapping("/javascript")
+    public String javascript(Model model) {
+        model.addAttribute("user", new User("userA", 10));
+        addUsers(model);
+
+        return "basic/javascript";
+    }
+
     private void addUsers(Model model) {
         ArrayList<User> users = new ArrayList<>();
         users.add(new User("userA", 10));
